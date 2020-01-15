414d6666084afe405f49ad390eefecb78630d206
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-15 10:08:43 -0600

    starter player and room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,9 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, direction):
+        self.name = name
+        self.direction = direction
+        # self.mood = mood
+        
