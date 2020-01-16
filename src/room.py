d2258f405ce58549b758c7372531a92afcb58a64
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-16 01:01:01 -0600

    added room items attribute

--- src/room.py
+++ src/room.py
@@ -2,9 +2,10 @@
 # description attributes.
 
 class Room:
-    def __init__(self, room_name, room_desc):
+    def __init__(self, room_name, room_desc, room_items = []):
         self.name = room_name
         self.description = room_desc
+        self.items = room_items
         
     def __repr__(self):
         return self.description
